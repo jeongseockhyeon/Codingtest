5c12c44235ea671e4ef17f93116d1491d18ec511
jeongseockhyeon <113079762+jeongseockhyeon@users.noreply.github.com>
2023-11-14 17:29:38 +0900

    [level unrated] Title: 공백으로 구분하기 1, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181869. 공백으로 구분하기 1/공백으로 구분하기 1.js
+++ 프로그래머스/unrated/181869. 공백으로 구분하기 1/공백으로 구분하기 1.js
@@ -5,7 +5,7 @@ function solution(my_string) {
     for ( let i = 0 ; i < Length ; i++ ){
         if(my_string[i] !== " ")
             str += my_string[i]
-        if (my_string[i+1] === " " || i === Length-1) {
+        if (my_string[i] === " " || i === Length-1) {
             answer.push(str)
             str = ""
         }
