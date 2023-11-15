aeb0ca7c8cb394b81a9e06cc8344b143ceccdd75
jeongseockhyeon <113079762+jeongseockhyeon@users.noreply.github.com>
2023-11-15 17:43:41 +0900

    [level unrated] Title: 문자열 바꿔서 찾기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.js
+++ 프로그래머스/unrated/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.js
@@ -2,12 +2,11 @@ function solution(myString, pat) {
     var answer = 0;
     let Length =myString.length
     const strArr = [...myString]
-    let changeArr = []
     for ( let i = 0 ; i < Length ; i++){
-        if( strArr[i] === 'A') changeArr[i] = 'B'
-        else changeArr[i] = 'A'
+        if( strArr[i] === 'A') strArr[i] = 'B'
+        else strArr[i] = 'A'
     }
-    const changeString = changeArr.join('')
+    const changeString = strArr.join('')
     changeString.includes(pat) ? answer = 1 : answer = 0
     return answer;
 }
