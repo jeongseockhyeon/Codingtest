1e07cc15944089624ae759527c469e05bccb1bda
jeongseockhyeon <113079762+jeongseockhyeon@users.noreply.github.com>
2023-11-22 18:34:50 +0900

    [level unrated] Title: 날짜 비교하기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181838. 날짜 비교하기/날짜 비교하기.js
+++ 프로그래머스/unrated/181838. 날짜 비교하기/날짜 비교하기.js
@@ -1,5 +1,6 @@
 function solution(date1, date2) {
     var answer = 0;
+    // return parseInt(date1) < parseInt(date2) ? 1 : 0
     if (date1[0] < date2[0]) return 1
     else if ( date1[0] > date2[0]) return 0
     else {
