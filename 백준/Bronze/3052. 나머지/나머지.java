c4f6ed1230584432a90fafe64ca98bee0908e797
jeongseockhyeon <113079762+jeongseockhyeon@users.noreply.github.com>
2025-09-26 00:03:04 +0900

    [Bronze II] Title: 나머지, Time: 172 ms, Memory: 17624 KB -BaekjoonHub

---
+++ 백준/Bronze/3052. 나머지/나머지.java
@@ -0,0 +1,17 @@
+import java.util.HashSet;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        HashSet<Integer> hashSet = new HashSet<>();
+
+        for (int i = 0; i < 10; i++) {
+            hashSet.add(sc.nextInt() % 42);
+        }
+
+        sc.close();
+        System.out.println(hashSet.size());
+    }
+}
