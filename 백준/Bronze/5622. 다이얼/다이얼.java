9d498d497c065c88033a134616cb7b110d5f7d22
jeongseockhyeon <113079762+jeongseockhyeon@users.noreply.github.com>
2025-10-19 16:04:06 +0900

    [Bronze II] Title: 다이얼, Time: 164 ms, Memory: 17540 KB -BaekjoonHub

---
+++ 백준/Bronze/5622. 다이얼/다이얼.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String str = sc.next();
+        int length = str.length();
+        int second = 0;
+
+        for (int i = 0; i < length; i++) {
+            switch (str.charAt(i)) {
+
+                case 'A' : case 'B' : case 'C' :
+                    second += 3;
+                    break;
+
+                case 'D' : case 'E': case 'F' :
+                    second += 4;
+                    break;
+
+                case 'G' : case 'H': case 'I' :
+                    second += 5;
+                    break;
+
+                case 'J' : case 'K': case 'L' :
+                    second += 6;
+                    break;
+
+                case 'M' : case 'N': case 'O' :
+                    second += 7;
+                    break;
+
+                case 'P' : case 'Q': case 'R' : case 'S' :
+                    second += 8;
+                    break;
+
+                case 'T' : case 'U': case 'V' :
+                    second += 9;
+                    break;
+
+                case 'W' : case 'X': case 'Y' : case 'Z' :
+                    second += 10;
+                    break;
+            }
+        }
+        System.out.println(second);
+    }
+}
