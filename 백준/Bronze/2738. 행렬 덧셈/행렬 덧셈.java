eb3015c928c27e9e4e2c58294f9887dee13bb880
jeongseockhyeon <113079762+jeongseockhyeon@users.noreply.github.com>
2025-11-05 23:26:59 +0900

    [Bronze III] Title: 행렬 덧셈, Time: 144 ms, Memory: 15852 KB -BaekjoonHub

--- 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.java
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.java
@@ -1,40 +1,34 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
 
-        int n = sc.nextInt();
-        int m = sc.nextInt();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
 
-        int[][] arr1 = generateArr(sc, n, m);
-        int[][] arr2 = generateArr(sc, n, m);
-
-        int[][] sumArr = new int[n][m];
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                sumArr[i][j] = arr1[i][j] + arr2[i][j];
-            }
-        }
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                System.out.print(sumArr[i][j] + " ");
-            }
-            System.out.println();
-        }
-        
-    }
-
-    public static int[][] generateArr(Scanner sc,int n, int m) {
         int[][] arr = new int[n][m];
 
         for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
             for (int j = 0; j < m; j++) {
-                arr[i][j] = sc.nextInt();
+                arr[i][j] = Integer.parseInt(st.nextToken());
             }
         }
-        return arr;
+        
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < m; j++) {
+                sb.append(arr[i][j] + Integer.parseInt(st.nextToken())).append(" ");
+            }
+            sb.append("\n");
+        }
+
+        System.out.print(sb);
     }
 }
